75ee9698a1fa7129c1acae3795d13c46b29d62a8
kevin knight <tgodkev@gmail.com>
2022-02-04 15:24:56 -0500

    started profile componenet

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface ProfileProps {
+name: string,
+photo: string,
+message: string
+
+}
+
+export const Profile: React.FC<ProfileProps> = ({props}) => {
+        return (
+            <div>
+                <h1>name</h1>
+                <h1> img src here </h1>
+                <h1>return messages this user has sent.</h1>
+            </div>
+        );
+}
